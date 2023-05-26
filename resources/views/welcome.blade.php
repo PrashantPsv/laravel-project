128ee32c672ce8871af27d732ce8cc09f129a911
subin <subinstark24@gmail.com>
2023-05-26 20:47:26 +0530

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -371,10 +371,10 @@
       <!-- end link -->
 
       <!-- link -->
-      <a href="./index-1.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+      {{-- <a href="./index-1.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
         <i class="fad fa-shopping-cart text-xs mr-2"></i>
         ecommerce dashboard
-      </a>
+      </a> --}}
       <!-- end link -->
 
       <p class="uppercase text-xs text-gray-600 mb-4 mt-4 tracking-wider">apps</p>
@@ -382,42 +382,46 @@
       <!-- link -->
       <a href="./email.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
         <i class="fad fa-envelope-open-text text-xs mr-2"></i>
-        email
+        Category
       </a>
       <!-- end link -->
 
       <!-- link -->
       <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
         <i class="fad fa-comments text-xs mr-2"></i>
-        chat
+        Sub-category
       </a>
       <!-- end link -->
 
       <!-- link -->
       <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
         <i class="fad fa-shield-check text-xs mr-2"></i>
-        todo
+        State
       </a>
       <!-- end link -->
 
       <!-- link -->
       <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
         <i class="fad fa-calendar-edit text-xs mr-2"></i>
-        calendar
+        City
       </a>
       <!-- end link -->
 
       <!-- link -->
       <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
         <i class="fad fa-file-invoice-dollar text-xs mr-2"></i>
-        invoice
+        Area
       </a>
       <!-- end link -->
 
       <!-- link -->
       <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
         <i class="fad fa-folder-open text-xs mr-2"></i>
-        file manager
+        Academy
+      </a>
+      <a href="#" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+        <i class="fad fa-folder-open text-xs mr-2"></i>
+        Teachers
       </a>
       <!-- end link -->   
       

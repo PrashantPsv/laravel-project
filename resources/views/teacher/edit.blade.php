61da1fb305e6d17684907088cc768d75bd70ef5e
subin <subinstark24@gmail.com>
2023-06-05 21:41:06 +0530

    last

--- resources/views/teacher/edit.blade.php
+++ resources/views/teacher/edit.blade.php
@@ -1,42 +1,37 @@
 @extends('welcome')
 @section('content')
-    <div class="container" >
-        <div class="row">
-            <div class="col-md-8 offset-md-2" style="width:720px;">
-                <div class="card">
-                    <div class="card-header">Add Teacher</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('addTeacher.Update',[$teacher->id]) }}">
-                            @csrf
-
-                            <div class="form-group">
-                                <label for="name"> First Name:</label>
-                                <input type="text" name="name" value="{{$teacher->name}}" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="lastname">Last Name:</label>
-                                <input type="text" name="lastname" value="{{$teacher->lastname}}" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email">Email:</label>
-                                <input type="email" name="email" value="{{$teacher->email}}" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Phone Number:</label>
-                                <input type="number" name="phone" value="{{$teacher->phone}}" class="form-control" required>
-                            </div>
-
-
-                            <button type="submit" class="btn btn-primary mt-2">Update Teacher</button>
-                        </form>
-                    </div>
-                </div>
-
+    <div class="container-xl" style="width:720px;">
+        <div class="card-header">
+            <h4 style="font-weight:600">Edit teacher</h4>
         </div>
 
-        </div>
+        <form method="POST" action="{{ route('addTeacher.Update', [$teacher->id]) }}">
+            @csrf
+
+            <div class="form-group">
+                <label for="name"> First Name:</label>
+                <input type="text" name="name" value="{{ $teacher->name }}" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="lastname">Last Name:</label>
+                <input type="text" name="lastname" value="{{ $teacher->lastname }}" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" value="{{ $teacher->email }}" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Phone Number:</label>
+                <input type="number" name="phone" value="{{ $teacher->phone }}" class="form-control" required>
+            </div>
+
+
+            <button type="submit" class="btn btn-primary mt-2">Update Teacher</button>
+        </form>
+
+
     </div>
+
 @endsection
--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,67 +1,66 @@
 @extends('welcome')
 @section('content')
-    <div class="container" >
-        <div class="row">
-            <div class="col-md-8 offset-md-2" style="width:720px;">
-                <div class="card">
-                    <div class="card-header">Add Teacher</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('addTeacher.store') }}">
-                            @csrf
-
-                            <div class="form-group">
-                                <label for="name"> First Name:</label>
-                                <input type="text" name="name" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="lastname">Last Name:</label>
-                                <input type="text" name="lastname" class="form-control" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email">Email:</label>
-                                <input type="email" name="email" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Phone Number:</label>
-                                <input type="number" name="phone" class="form-control" required>
-                            </div>
+    <div class="container-xl" style="width:720px;">
+        <div class="card-header">
+            <h4 style="font-weight:600">Add teacher</h4>
+        </div>
 
 
-                            <button type="submit" class="btn btn-primary mt-2">Add Teacher</button>
-                        </form>
-                    </div>
-                </div>
-                <div class="pt-5">
-                    <table class=" table table-bordered table-striped table-hover">
+        <form method="POST" action="{{ route('addTeacher.store') }}">
+            @csrf
 
-                    <thead>
-                        <tr>
-                            <th>
-                                id
-                            </th>
-                            <th>
-                                First Name
-                            </th>
-                            <th>
-                                Last Name
-                            </th>
-                            <th>
-                                Email
-                            </th>
-                            <th>
-                                Phone Number
-                            </th>
+            <div class="form-group">
+                <label for="name"> First Name:</label>
+                <input type="text" name="name" class="form-control" required>
+            </div>
 
-                            <th>
-                                Action
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($teachers as $teacher)
+            <div class="form-group">
+                <label for="lastname">Last Name:</label>
+                <input type="text" name="lastname" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Phone Number:</label>
+                <input type="number" name="phone" class="form-control" required>
+            </div>
+
+
+            <button type="submit" class="btn btn-primary mt-2">Add Teacher</button>
+        </form>
+
+
+        <div class="pt-5">
+            <table class=" table table-bordered table-striped table-hover">
+
+                <thead>
+                    <tr>
+                        <th>
+                            id
+                        </th>
+                        <th>
+                            First Name
+                        </th>
+                        <th>
+                            Last Name
+                        </th>
+                        <th>
+                            Email
+                        </th>
+                        <th>
+                            Phone Number
+                        </th>
+
+                        <th>
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($teachers as $teacher)
                         <tr>
                             <td>
                                 {{ $teacher->id ?? '' }}
@@ -82,24 +81,22 @@
                                 <a class="btn btn-xs btn-primary p-0" href="{{ route('addTeacher.edit', [$teacher->id]) }}">
                                     Edit
                                 </a>
-                                <a class="btn btn-xs btn-danger p-0" href="{{ route('addTeacher.delete', [$teacher->id]) }}">
+                                <a class="btn btn-xs btn-danger p-0"
+                                    href="{{ route('addTeacher.delete', [$teacher->id]) }}">
                                     Delete
                                 </a>
                             </td>
                         </tr>
-                        @empty
+                    @empty
                         <tr>
                             <td colspan="3">
                                 No Data Found
                             </td>
                         </tr>
-                        @endforelse
-                    </tbody>
+                    @endforelse
+                </tbody>
 
             </table>
-            </div>
-        </div>
-
         </div>
     </div>
-@endsection
+    @endsection
